9007abc1c2884336aeb4ae8106311a6be40178eb
Ishuman <ishuman2006@gmail.com>
2026-02-28 03:13:26 +0530

    Fix fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
 import './App.css'
 import { useEffect, useState } from 'react';
 
-async function getPokemonSprite (pokemonName: string) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+type SpritesObject = {
+  back_default: string;
+  back_shiny: string;
+  front_default: string;
+  front_shiny: string
+}
+
+async function getPokemonSprite (pokemonName: string){
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`, {
+    method: "GET"
+  });
   return await response.json();
 }
 
-function App() {
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
+function App() {
   // no deps for now -- using hardcoded value rayquaza for this example
-  // TODO: setup a cleanup function for this effect. It would (in the actual example) need to run only once per render.
-  // Fix the infinite loop!
-  const [mySprites, setMySprites] = useState([]);
+  // do I need a cleanup function for this?
+  const [mySprites, setMySprites] = useState<SpritesObject>({back_default: "", back_shiny: "", front_default: "", front_shiny: ""});
 
   useEffect(() => {
     getPokemonSprite("rayquaza").then((pokeJSON) => {
-      console.log(pokeJSON);
-      console.log(pokeJSON.sprites);
       // will I have to call populate page from this effect?
+      console.log("PokeJSON is as follows:");
+      console.log(pokeJSON);
       setMySprites(pokeJSON.sprites);
-    }, [mySprites]);
-  })
+    });
 
-  console.log(mySprites);
+    return;
+  }, [])
 
   return (
     <>
       <div className='text-3xl font-bold'>This is my ultracool pokeAPI game thing</div>
+      <img src={mySprites.front_shiny} alt="The best pokemon on planet Earth my favorite my lovely rayquaza" />
     </>
   )
 }
